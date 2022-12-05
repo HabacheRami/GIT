6a698b127d87e6f45b8aca1230f5e1f31c8c1d67
Rami <habache.rami@gmail.com>
2022-12-05 23:47:15 +0100

    Add Key SSH

--- TP1/script.sh
+++ TP1/script.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-
-# Copier le fichier de configuration sur le serveur
+#copier le fichier de configuration sur le serveur
 
 cp ./config__files/ssh/sshd_config /etc/ssh/sshd_config
 
 cp ./config__files/ssh/Banner /etc/Banner
 
 
-# Redemarrer le service sshd 
+#Redemarrer le service sshd
 
-systemctl restart sshd 
+systemctl restart sshd
 
-# Création d'utilisateur
+#Création d'utilisateur
+
 printf "Donnez le nom de l'utilisateur à créer :\n"
+
 read user
+
 useradd -m -d/home/$user -s /bin/bash -c "$user" $user && passwd $user && usermod -aG sudo $user && echo -e $user "est devenu un superUser !\n"
 
-# Insertion de la clé public 
+
+#Insertion de la clé public
 
 printf "Entrez votre clé public : \n"
 
