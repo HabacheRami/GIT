18e94ba2611f900311abf433a4db5e8fd3df0154
Rami <habache.rami@gmail.com>
2022-12-05 22:56:04 +0100

    Upadate test connexion ssh

--- TP1/script.sh
+++ TP1/script.sh
@@ -17,4 +17,12 @@ printf "Donnez le nom de l'utilisateur à créer :\n"
 read user
 useradd -m -d/home/$user -s /bin/bash -c "$user" $user && passwd $user && usermod -aG sudo $user && echo -e $user "est devenu un superUser !\n"
 
+# Insertion de la clé public 
 
+printf "Entrez votre clé public : \n"
+
+read key
+
+echo $key >> tmp.txt
+
+cat tmp.txt >> ~/.ssh/authorized_keys && 
